fb756928430696d4102c44691fc3c41655cf8e7f
Bansari Patel <122435714+Bansari88@users.noreply.github.com>
2023-03-03 13:15:49 -0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ import '../CSS/Footer.css'
 export default function Footer(){
     return(
         <div className="footer">
-            <a href='https://github.com/BansariPatel1' target="_blank"><img src={githubLogo} className='footer--firstImg'></img></a>
+            <a href='https://github.com/Bansari88' target="_blank"><img src={githubLogo} className='footer--firstImg'></img></a>
             <a href='https://twitter.com/bpatel0721' target="_blank"><img src={twitterLogo} className='footer--secondImg'></img></a>
-            <a href='https://www.facebook.com/profile.php?id=100089169651871' target="_blank"><img src={facebookLogo} className='footer--thirdImg'></img></a>
+            <a href='https://www.facebook.com' target="_blank"><img src={facebookLogo} className='footer--thirdImg'></img></a>
         </div>
     )
 }
